82c15dc6ea5e9409a09ad6ea31ef01d3363c832c
DY-L <dyllanwli@users.noreply.github.com>
2020-03-22 16:50:21 -0500

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,19 @@
+module purge
+module load Anaconda/3-5.0.0.1
+conda create -n jupyterlab_1.2.2
+
+
+# once created you will see
+#
+# To activate this environment, use:
+# > source activate jupyterlab_1.2.2
+#
+# To deactivate an active environment, use:
+# > source deactivate
+#
+# then add package to jupyterlab
+
+
+source activate jupyterlab_1.2.2
+conda install -c conda-forge jupyterlab=1.2.2
+conda install --file requirements.txt
